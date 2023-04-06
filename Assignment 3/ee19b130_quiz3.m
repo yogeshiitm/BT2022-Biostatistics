a126652e88b19a81451c3631543149bccbf8bb92
Yogesh Agarwala <69758664+yogeshiitm@users.noreply.github.com>
2023-04-06 21:36:13 +0530

    assign 3

---
+++ Assignment 3/ee19b130_quiz3.m
@@ -0,0 +1,9 @@
+% p-value for the replication F-test (5 marks)
+% F-test status on replication component (2 mark)
+% ---
+% p-valuer for the treatment F-test (5 marks)
+% F-test status on treatment component (2 mark)
+% ---
+% p-value for the post ANOVA t-test pair 1 (3 marks)
+% p-value for the post ANOVA t-test pair 2 (3 marks)
+
